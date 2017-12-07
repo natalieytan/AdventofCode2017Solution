d75dd8db3ad4da98141681e573b37afc7fd97921
Natalie Tan <natalieytan@gmail.com>
2017-12-07 22:10:34 +1100

    Day 7 solutions, Corrected indentation on  Day 5 Solution

--- day5.py
+++ day5.py
@@ -1,23 +1,24 @@
 
-    with open("day5input.txt") as open_file:
-        data = open_file.read().splitlines()
+with open("day5input.txt") as open_file:
+    data = open_file.read().splitlines()
 
-    def count_steps(part2, input):
-        steps = list(map(int, input))
-        goal = len(steps)
-        position = 0
-        count = 0
+def count_steps(part2, input):
+    steps = list(map(int, input))
+    print(len(steps))
+    goal = len(steps)
+    position = 0
+    count = 0
 
-        while position < goal:
-            instruction = steps[position]
-            if part2 and instruction >=3:
-                steps[position] -= 1
-            else:
-                steps[position] += 1
-            position += instruction
-            count+=1
+    while position < goal:
+        instruction = steps[position]
+        if part2 and instruction >=3:
+            steps[position] -= 1
+        else:
+            steps[position] += 1
+        position += instruction
+        count+=1
 
-        return count
+    return count
 
-    print('Part 1 :' + str(count_steps(False, data)))
-    print('Part 2 :' + str(count_steps(True, data)))
+print('Part 1 :' + str(count_steps(False, data)))
+#print('Part 2 :' + str(count_steps(True, data)))
